cee6b1f2ffb3f1f80d4e5fd2efda26165ff11cfa
Daniel Malone <dmalone@redfaire.com>
2017-06-12 12:11:06 +0100

    Preparing for merge

--- vueanalyser/src/main.js
+++ vueanalyser/src/main.js
@@ -1,29 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-// import Chart from 'chartjs';
-// import dt from 'datatables.net-bs'
-// import dtbuttons from 'datatables.net-buttons-bs'
-// import dtselect from 'datatables.net-select'
-import ElementUI from 'element-ui'
-import DataTables from 'vue-data-tables'
-
-
-
-// global.jQuery = jQuery;
-
 import HeaderComp from './Components/headComponent.vue';
-import Chart from './Components/Charts.vue';
 import SidebarComp from './Components/SidebarComponent.vue';
 import SidebarOverlayComp from './Components/SidebarOverlayComponent.vue';
 import FooterComp from './Components/FooterComponent.vue';
 import ChartsComp from './Components/ChartsComponent.vue';
 import AnalyticsHistory from './Components/AnalyticsHistory.vue';
 import Connections from './Components/Connections.vue';
+import Test from './Components/Test.vue';
+
 
 Vue.use(VueRouter);
-Vue.use(ElementUI)
-Vue.use(DataTables)
 
 Vue.component('headComponent', HeaderComp);
 Vue.component('sidebarComp', SidebarComp);
@@ -32,7 +20,9 @@ Vue.component('footerComp', FooterComp);
 Vue.component('chartsComp', ChartsComp);
 Vue.component('analyticsHistory', AnalyticsHistory);
 Vue.component('connections', Connections);
-Vue.component('charts', Chart);
+Vue.component('test', Test);
+
+
 
 const router = new VueRouter({
     routes: [
